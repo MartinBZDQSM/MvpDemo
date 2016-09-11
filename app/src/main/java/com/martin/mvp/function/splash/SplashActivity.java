fc68432dfa353e8c16685f663cf98797be69f766
MartinBZDQSM <569835826@qq.com>
2016-09-12 00:11:46 +0800

    add utils

---
+++ app/src/main/java/com/martin/mvp/function/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.martin.mvp.function.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.martin.mvp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
