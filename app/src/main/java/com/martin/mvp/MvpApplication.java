c7f416d02014eff687f0faad10c9d403576c861b
MartinBZDQSM <569835826@qq.com>
2016-09-19 11:49:04 +0800

    normal up

--- app/src/main/java/com/martin/mvp/MvpApplication.java
+++ app/src/main/java/com/martin/mvp/MvpApplication.java
@@ -3,6 +3,8 @@ package com.martin.mvp;
 import android.app.Application;
 import android.content.Context;
 
+import com.martin.mvp.common.di.component.ApplicationComponent;
+
 /**
  * 作者：MartinBZDQSM on 2016/9/14 0014.
  * 博客：http://www.jianshu.com/users/78f0e5f4a403/latest_articles
@@ -12,14 +14,28 @@ public class MvpApplication extends Application {
 
     private static Context mContext;
 
+    private static ApplicationComponent applicationComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
         mContext = this;
+        applicationComponentInit();
+
+    }
+
+    private void applicationComponentInit() {
+
+
     }
 
     public static Context getmContext() {
         return mContext;
+    }
+
+    public static ApplicationComponent getApplicationComponent(){
+        return applicationComponent;
     }
 
 }
